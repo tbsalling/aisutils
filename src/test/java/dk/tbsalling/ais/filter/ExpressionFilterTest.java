87421022bd4cdaa5dca0c8d3ae892652b494f2c8
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-11-13 16:33:40 +0100

    Added unit tests

--- src/test/java/dk/tbsalling/ais/filter/ExpressionFilterTest.java
+++ src/test/java/dk/tbsalling/ais/filter/ExpressionFilterTest.java
@@ -7,9 +7,11 @@ import dk.tbsalling.aismessages.nmea.messages.NMEAMessage;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static org.junit.Assert.assertFalse;
@@ -17,38 +19,57 @@ import static org.junit.Assert.assertTrue;
 
 public class ExpressionFilterTest {
 
+    //
+    // Test eq, neq, lt, lte, gt, gte
+    //
+
     @Test
-    public void testMsgIdOrMmsi() throws Exception {
-        Predicate<AISMessage> expressionFilter = FilterFactory.newExpressionFilter("msgid=1 or mmsi=227006760");
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResource("ais-sample-1.nmea").openStream();
-
-        final boolean[] weSawTrueResults = {false};
-        final boolean[] weSawFalseResults = {false};
-
-        processAISInputStream(inputStream, msg -> {
-            try {
-                boolean test = expressionFilter.test(msg);
-                if (msg.getMessageType().getCode() == 1 || msg.getSourceMmsi().getMMSI() == 227006760) {
-                    assertTrue(test);
-                    weSawTrueResults[0] = true;
-                }
-                else {
-                    assertFalse(test);
-                    weSawFalseResults[0] = true;
-                }
-            } catch (IllegalArgumentException e) {
-                System.err.println(msg.getSourceMmsi() + ": " + e.getMessage());
-            }
-        });
-
-        assertTrue(weSawTrueResults[0]);
-        assertTrue(weSawFalseResults[0]);
+    public void testMsgIdEquals() throws Exception {
+        verifyExpressionFilter("msgid=3", msg -> msg.getMessageType().getCode() == 3);
     }
 
     @Test
-    public void testMsgIdAndMmsi() throws Exception {
-        Predicate<AISMessage> expressionFilter = FilterFactory.newExpressionFilter("msgid=1 and mmsi=227006760");
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResource("ais-sample-1.nmea").openStream();
+    public void testMsgIdNotEquals() throws Exception {
+        verifyExpressionFilter("msgid!=3", msg -> msg.getMessageType().getCode() != 3);
+    }
+
+    @Test
+    public void testMsgIdLessThan() throws Exception {
+        verifyExpressionFilter("msgid<3", msg -> msg.getMessageType().getCode() < 3);
+    }
+
+    @Test
+    public void testMsgIdLessThanEquals() throws Exception {
+        verifyExpressionFilter("msgid<=3", msg -> msg.getMessageType().getCode() <= 3);
+    }
+
+    @Test
+    public void testMsgIdGreaterThan() throws Exception {
+        verifyExpressionFilter("msgid>3", msg -> msg.getMessageType().getCode() > 3);
+    }
+
+    @Test
+    public void testMsgIdGreaterThanEquals() throws Exception {
+        verifyExpressionFilter("msgid>=3", msg -> msg.getMessageType().getCode() >= 3);
+    }
+
+    //
+    // Test and/or operator
+    //
+
+    @Test
+    public void testMsgIdEqualsOrMmsiEquals() throws Exception {
+        verifyExpressionFilter("msgid=1 or mmsi=227006760", msg -> msg.getMessageType().getCode() == 1 || msg.getSourceMmsi().getMMSI() == 227006760);
+    }
+
+    @Test
+    public void testMsgIdEqualsAndMmsiEquals() throws Exception {
+        verifyExpressionFilter("msgid=1 and mmsi=227006760", msg -> msg.getMessageType().getCode() == 1 && msg.getSourceMmsi().getMMSI() == 227006760);
+    }
+
+    private static void verifyExpressionFilter(String filterExpression, Predicate<AISMessage> verification) throws Exception {
+        final Predicate<AISMessage> expressionFilter = FilterFactory.newExpressionFilter(filterExpression);
+        final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResource("ais-sample-1.nmea").openStream();
 
         final boolean[] weSawTrueResults = {false};
         final boolean[] weSawFalseResults = {false};
@@ -56,11 +77,10 @@ public class ExpressionFilterTest {
         processAISInputStream(inputStream, msg -> {
             try {
                 boolean test = expressionFilter.test(msg);
-                if (msg.getMessageType().getCode() == 1 && msg.getSourceMmsi().getMMSI() == 227006760) {
+                if (verification.test(msg)) {
                     assertTrue(test);
                     weSawTrueResults[0] = true;
-                }
-                else {
+                } else {
                     assertFalse(test);
                     weSawFalseResults[0] = true;
                 }
