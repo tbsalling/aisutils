81ab1fcd9f4d3b8f794cc23256cde5bc6ae40868
BORDAC Tomas <tbordac@frequentis.com>
2021-12-20 15:01:32 +0100

    insert/update aidToNavigationReport into the tracker

--- src/test/java/dk/tbsalling/ais/tracker/AISTrackerTest.java
+++ src/test/java/dk/tbsalling/ais/tracker/AISTrackerTest.java
@@ -42,7 +42,7 @@ public class AISTrackerTest {
 
     @Test
     public void testGetNumberOfAisTracks() {
-        assertEquals(916, tracker.getNumberOfAisTracks());
+        assertEquals(922, tracker.getNumberOfAisTracks());
     }
 
     @Test
@@ -77,7 +77,7 @@ public class AISTrackerTest {
     @Test
     public void testGetAisTracks() throws Exception {
         Set<AISTrack> aisTracks = tracker.getAisTracks();
-        assertEquals(916, aisTracks.size());
+        assertEquals(922, aisTracks.size());
 
         final boolean[] found = {false};
         aisTracks.forEach(track -> { if (track.getMmsi() == 236037000) found[0] = true; });
