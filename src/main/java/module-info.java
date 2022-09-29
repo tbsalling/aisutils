d5d8f4c03c73d2ae935116d84696f41363edb7ae
Thomas Borg Salling <tbsalling@tbsalling.dk>
2022-09-29 21:01:19 +0200

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module dk.tbsalling.ais.utils {
+    requires com.google.common;
+    requires dk.tbsalling.ais.messages;
+    requires jsr305;
+    requires org.antlr.antlr4.runtime;
+    requires slf4j.api;
+
+    exports dk.tbsalling.ais.filter;
+    exports dk.tbsalling.ais.tracker.events;
+    exports dk.tbsalling.ais.tracker;
+
+    opens dk.tbsalling.ais.tracker to com.google.common;
+}
